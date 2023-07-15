4ba6bf7a3bb33147b7b7c52061ae24f8f38df5d0
Albert on wsl <albertirura11@gmail.com>
2023-07-15 14:35:37 +0300

    User Class

--- models/user.py
+++ models/user.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
+"""Module defines one class - User"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """Defines a User"""
-
     email = ""
     password = ""
     first_name = ""
