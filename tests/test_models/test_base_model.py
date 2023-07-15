b2e440070351a77e755cc554ed801d7373d0c9b0
Albert on wsl <albertirura11@gmail.com>
2023-07-15 13:09:35 +0300

    BaseModel unittest

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,3 +15,26 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(obj.updated_at, datetime.datetime)
         self.assertIsInstance(obj.id, str)
 
+    def test_str(self):
+        """Test str magic method"""
+        obj = BaseModel()
+        self.assertTrue(str(obj), str)
+
+    def test_save(self):
+        """Test save method"""
+        obj = BaseModel()
+        updated_at = obj.updated_at
+        obj.save()
+        updated_at_2 = obj.updated_at
+        self.assertTrue(updated_at < updated_at_2)
+
+    def test_to_dict(self):
+        """Test to_dict method"""
+        obj = BaseModel()
+        objDict = obj.to_dict()
+        self.assertIsInstance(objDict, dict)
+        self.assertIsInstance(objDict['updated_at'], str)
+        self.assertIsInstance(objDict['created_at'], str)
+        self.assertIsInstance(objDict['id'], str)
+        self.assertEqual(objDict['__class__'], "BaseModel")
+
